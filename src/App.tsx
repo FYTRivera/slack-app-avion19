a683b5707d2a3d7058c9db07d4f5ff308f88cecb
Junmark Chi <chijunmark97@gmail.com>
2022-07-06 23:53:18 +0800

    Push recent updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard/index";
+import Signin from "./components/signin";
 import Signup from "./components/signup";
+import PrivateRoutes from "./privateRoutes";
 import "./styles/app.css";
 
-function App() {
+const App: React.FC = () => {
+  localStorage.setItem("isLoggedIn", JSON.stringify(true));
   return (
-    <div className="App">
-      <Signup />
-    </div>
+    <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 
-export default function Signin(){
-    return(
-        <div className="signin">
-            <img src={logo}></img>
-        <h1>Sign in to your account</h1>
+const Signin: React.FC = () => {
+  return (
+    <div className="signin">
+      <img src={logo}></img>
+      <h1>Sign in to your account</h1>
       <form action="">
         <input className="email" type="email" placeholder="Email Address" />
         <input className="password" type="password" placeholder="Password" />
@@ -13,9 +14,10 @@ export default function Signin(){
       </form>
       <div className="footer">
         <h6>New to Slack?</h6>
-        <a href="#">Sign up to an existing account</a>
+        <Link to="/signup">Sign up to an existing account</Link>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
+export default Signin;
