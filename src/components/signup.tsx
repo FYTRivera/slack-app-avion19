7b6eb4913155f684a70df94324bdff9ae2f1cc4f
Junmark Chi <chijunmark97@gmail.com>
2022-07-06 11:37:14 +0800

    Add signup component

---
+++ src/components/signup.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import "../styles/signup.css";
+
+interface SignupProps {
+  e: Event;
+}
+
+export default function Signup() {
+  const handleSubmit = ({ e }: SignupProps) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signup">
+      <img src={logo}></img>
+      <h1>Create your new account</h1>
+      <h4>We suggest using the email address you use at work.</h4>
+      <div className="buttons">
+        <button className="google-signup">
+          <i className="fa-brands fa-google"></i>
+          Continue with Google
+        </button>
+        <button className="facebook-signup">
+          <i className="fa-brands fa-facebook"></i>
+          Continue with Facebook
+        </button>
+      </div>
+      <h5>OR</h5>
+      <form action="">
+        <input className="fullname" type="text" placeholder="Full Name" />
+        <input className="email" type="email" placeholder="Email Address" />
+        <input className="password" type="password" placeholder="Password" />
+        <input
+          className="confirm-password"
+          type="password"
+          placeholder="Confirm password"
+        />
+        <input className="submit" type="submit" value="Create Account" />
+      </form>
+      <div className="footer">
+        <h6>Already using Slack?</h6>
+        <a href="#">Sign in to an existing account</a>
+      </div>
+    </div>
+  );
+}
