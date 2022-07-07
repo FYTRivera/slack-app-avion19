de184bd4b0d949afd1c4d8b30014a8dfdd0e1f93
Junmark Chi <Chijunmark97@gmail.com>
2022-07-07 09:02:54 +0800

    Update files

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import "../styles/signin.css";
 
 const Signin: React.FC = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signin">
       <img src={logo}></img>
-      <h1>Sign in to your account</h1>
+      <h1>Sign in to Slack</h1>
       <form action="">
         <input className="email" type="email" placeholder="Email Address" />
         <input className="password" type="password" placeholder="Password" />
-        <input className="submit" type="submit" value="Create Account" />
+        <input className="submit" type="submit" value="Sign In" />
       </form>
       <div className="footer">
         <h6>New to Slack?</h6>
-        <Link to="/signup">Sign up to an existing account</Link>
+        <Link to="/signup">Sign up for a new account</Link>
       </div>
     </div>
   );
